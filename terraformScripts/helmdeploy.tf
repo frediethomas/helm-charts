ecb4887d237c71f0dfc94b79bf4027afb6e715e8
fredie <frediethomas@gmail.com>
2020-05-08 03:47:41 -0700

    terraformScripts

---
+++ terraformScripts/helmdeploy.tf
@@ -0,0 +1,83 @@
+data "helm_repository" "poc" {
+  name = "poc"
+  url  = "https://frediethomas.github.io/helm-charts"
+}
+
+resource "helm_release" "redis" {
+  name       = "redis-poc"
+  repository = data.helm_repository.poc.metadata[0].name
+  chart      = "redis"
+  version    = "0.1.0"
+}
+
+
+
+resource "helm_release" "adservice" {
+  name       = "adservice-poc"
+  repository = data.helm_repository.poc.metadata[0].name
+  chart      = "adservice"
+  version    = "0.1.0"
+}
+
+resource "helm_release" "cartservice" {
+  name       = "cartservice-poc"
+  repository = data.helm_repository.poc.metadata[0].name
+  chart      = "cartservice"
+  version    = "0.1.0"
+}
+
+resource "helm_release" "checkoutservice" {
+  name       = "checkoutservice-poc"
+  repository = data.helm_repository.poc.metadata[0].name
+  chart      = "checkoutservice"
+  version    = "0.1.0"
+}
+resource "helm_release" "currencyservice" {
+  name       = "currencyservice-poc"
+  repository = data.helm_repository.poc.metadata[0].name
+  chart      = "currencyservice"
+  version    = "0.1.0"
+}
+
+
+resource "helm_release" "emailservice" {
+  name       = "emailservice-poc"
+  repository = data.helm_repository.poc.metadata[0].name
+  chart      = "emailservice"
+  version    = "0.1.0"
+}
+resource "helm_release" "frontend" {
+  name       = "frontend-poc"
+  repository = data.helm_repository.poc.metadata[0].name
+  chart      = "frontend"
+  version    = "0.1.0"
+}
+
+resource "helm_release" "paymentservice" {
+  name       = "paymentservice-poc"
+  repository = data.helm_repository.poc.metadata[0].name
+  chart      = "paymentservice"
+  version    = "0.1.0"
+}
+
+resource "helm_release" "productcatalogservice" {
+  name       = "productcatalogservice-poc"
+  repository = data.helm_repository.poc.metadata[0].name
+  chart      = "productcatalogservice"
+  version    = "0.1.0"
+}
+
+resource "helm_release" "recommendationservice" {
+  name       = "recommendationservice-poc"
+  repository = data.helm_repository.poc.metadata[0].name
+  chart      = "recommendationservice"
+  version    = "0.1.0"
+}
+
+resource "helm_release" "shippingservice" {
+  name       = "shippingservice-poc"
+  repository = data.helm_repository.poc.metadata[0].name
+  chart      = "shippingservice"
+  version    = "0.1.0"
+}
+
---
+++ terraformScripts/readme.txt
@@ -0,0 +1,11 @@
+helmdeploy.tf
+---------------
+steps to run this on a glcoud shell
+1. open shell having a cluster attached.if no cluster,create and then connect to it
+
+2. since terraform comes preinstalled with gcloud dontwant to install.else install terraform
+
+3. run "terraform apply --auto-approve"
+      pods will get applied..if timeout..re run..
+    
+
